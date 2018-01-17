6ca33ec370148e278b9a4809d27e9f5dd8ef210f
Christophe Villeger <villeger.c@gmail.com>
2018-01-17 02:41:19 +0100

    init

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+$(function() {
+    $.get( "/entries", function( entries ) {
+        $('.no-records-found').remove()
+        var html = '';
+        entries.forEach(function( entry ) {
+            html =
+                '<tr>'
+                + '<td>' + entry.name + '</td>'
+                + '<td>' + entry.waitTime.postedWaitMinutes + '</td>'
+                + '<td>' + entry.waitTime.fastPass.available + '</td>'
+                + '<td>' + entry.waitTime.singleRider + '</td>'
+                + '<td>' + entry.waitTime.status + '</td>'
+                + '</tr>'
+            ;
+            $( "tbody" ).append(html);
+        });
+    });
+});
--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,12 +2,60 @@
 
 namespace App\Controller;
 
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class HomepageController extends Controller
 {
-    public function index()
+    public function index(Request $request): Response
     {
+        $client = new Client();
+
+        $accessToken = $request->getSession()->get('access_token');
+        if (empty($accessToken)){
+            $response = $client->post('https://authorization.go.com/token', [
+                'headers' => [
+                    'Content-Type' => 'application/x-www-form-urlencoded'
+                ],
+                'body' => 'grant_type=assertion&assertion_type=public&client_id=WDPRO-MOBILE.MDX.WDW.ANDROID-PROD'
+            ]);
+            if ($response->getStatusCode() !== 200){
+                throw new HttpException(418);
+            }
+
+            $json = json_decode($response->getBody()->getContents());
+
+            $request->getSession()->invalidate();
+            $request->getSession()->set('access_token', $json->access_token);
+        }
+
         return $this->render('homepage/index.html.twig');
+    }
+
+    public function entries(Request $request): JsonResponse
+    {
+        $accessToken = $request->getSession()->get('access_token');
+        if (empty($accessToken)){
+            return $this->redirectToRoute('homepage');
+        }
+
+        $client = new Client();
+        $response = $client->get('https://api.wdpro.disney.go.com/facility-service/theme-parks/P1;destination=dlp/wait-times?region=fr', [
+            'headers' => [
+                'Authorization' => "Bearer $accessToken"
+            ]
+        ]);
+
+        if ($response->getStatusCode() !== 200){
+            throw new HttpException(418);
+        }
+
+        $json = json_decode($response->getBody()->getContents());
+
+        return new JsonResponse($json->entries);
     }
 }
