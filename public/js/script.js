d995a709ce0bafdb453aa2e71b85255b211c967d
Christophe Villeger <villeger.c@gmail.com>
2018-01-18 02:40:04 +0100

    init

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ $(function() {
     google.charts.load('current', {'packages': ['table']});
     google.charts.setOnLoadCallback(drawTable);
     function drawTable() {
-        $.get( "/entries", function( jsonData ) {
+        $.get( "/index.php/entries", function( jsonData ) {
             var data = new google.visualization.DataTable(jsonData);
             var table = new google.visualization.Table(document.getElementById('table_div'));
             table.draw(data, {width: '100%', height: '100%'});
